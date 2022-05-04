7d962904be5e27dd9d5c6a8ccee13db0374f6ba2
taret92 <ivancho92.10@hotmail.com>
2022-05-04 00:26:53 -0500

    task 15

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def remove_char_at(str, n):
+    remove = ""
+    for i in range(len(str)):
+        if i == n:
+            remove = remove.replace(str[i], )
+        remove = remove + str[i]
+        return (remove)
