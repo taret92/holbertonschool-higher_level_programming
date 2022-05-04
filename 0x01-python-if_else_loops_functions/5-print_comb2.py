54307e6290d04883b8ebc356cba441baa39ff7e8
taret92 <ivancho92.10@hotmail.com>
2022-05-03 22:22:13 -0500

    task 5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 for number in range(0, 100):
-    if number <= 100:
-        print(f"{number:02d},", end=" ")
+    if number == 99:
+        print(f"{number:02d}")
+    else:
+        print(f"{number:02d}", end=", ")
