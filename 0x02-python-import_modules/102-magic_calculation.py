f65c13a4ad20786887d8edc8d02870c58d25a147
taret92 <ivancho92.10@hotmail.com>
2022-05-05 23:23:42 -0500

    task 102

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-def magic_calculation_102 (a, b):
+def magic_calculation102(a, b):
     from magic_calculation_102 import add, sub
     if a < b:
         c = add(a, b)
-    for i in range(4 ,6):
+    for i in range(4, 6):
         c = add(c, i)
         return(c)
     else:
