4be495349eebb899f1a9064bb27f3763e68eb8ab
taret92 <ivancho92.10@hotmail.com>
2022-05-09 19:26:34 -0500

    task 1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-from asyncio.windows_events import NULL
 
 def element_at(my_list, idx):
 
-    if (idx < 0 or idx >= len(my_list)):
-        return(None)
+    if idx < 0 or idx >= len(my_list):
+        return (None)
     else:
-        return(my_list[idx])
+        return (my_list[idx])
