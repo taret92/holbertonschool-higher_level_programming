4602950a48165810ece684085875376a8617b606
taret92 <ivancho92.10@hotmail.com>
2022-05-09 19:45:11 -0500

    task 2

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -2,9 +2,5 @@
 
 def replace_in_list(my_list, idx, element):
     if idx == 0 or idx > my_list:
-        return (my_list)
-
-    else:
-        for idx in range(len(my_list)):
-            if my_list[idx] == idx:
-                my_list[idx] == element
+        my_list[idx] == element
+        
