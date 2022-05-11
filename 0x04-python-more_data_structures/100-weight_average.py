1330561e2c67eaa23f2f24fa0a5d1c7fea8d633f
taret92 <ivancho92.10@hotmail.com>
2022-05-10 23:49:57 -0500

    task 100

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,10 +2,11 @@
 def weight_average(my_list=[]):
     if not my_list:
         return 0
+
     score = 0
     weight = 0
     for tuple in my_list:
         i, j = tuple
-        score = score + i*j
-        weight = weight + j
+        score += i*j
+        weight += j
     return score/weight
