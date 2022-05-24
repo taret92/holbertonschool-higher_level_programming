26be37e82167924b1692963440e71723729b54d8
taret92 <ivancho92.10@hotmail.com>
2022-05-23 19:52:49 -0500

    task 0 fixed

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,12 @@
 #!/bin/python3
 
-from distutils.log import error
-
 def safe_print_list(my_list=[], x=0):
-
-    my_list in range([":x"])
-    try:
-        print("{my_list")
-    except IndexError:
-        print()
-    return my_list
+    number = 0
+    for i in range([x]):
+        try:
+            print("{}".format(my_list[i]),end="") 
+            number += 1
+        except IndexError:
+            break
+        print("")
+    return (number)
