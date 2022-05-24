2ce38330a3c9e7b90236fc5ce7110e8c6e7e4333
taret92 <ivancho92.10@hotmail.com>
2022-05-23 19:35:02 -0500

    task 0 and readme

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/bin/python3
+
+from distutils.log import error
+
+def safe_print_list(my_list=[], x=0):
+
+    my_list in range([":x"])
+    try:
+        print("{my_list")
+    except IndexError:
+        print()
+    return my_list
