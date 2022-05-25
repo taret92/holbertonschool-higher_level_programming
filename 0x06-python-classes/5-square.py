de69a7c236323d292ace43892d4d1fc6fef4fcd1
taret92 <ivancho92.10@hotmail.com>
2022-05-25 13:16:07 -0500

    task 5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""Create a empty class"""
+
+
+class Square:
+    """square with size"""
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        """getting size atributte"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """adding value to size atribute"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """area of the operation"""
+        return self.__size ** 2
+
+    def my_print(self):
+        area = self.area()
+        size = self.size
+        for i in range(area):
+            if i % size == 0 and i != 0:
+                print()
+        else:
+            print("#", end="")
+        print()
