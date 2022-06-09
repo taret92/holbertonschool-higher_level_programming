fc722991033003c8709033c19179733891941e97
taret92 <ivancho92.10@hotmail.com>
2022-06-08 19:58:57 -0500

    task 2

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Appends text to file"""
+
+
+def append_write(filename="", text=""):
+    """Appends text to file with open function"""
+
+    with open(filename, "w") as f:
+        my_file = f.write(text)
+        f.close()
+        return my_file
