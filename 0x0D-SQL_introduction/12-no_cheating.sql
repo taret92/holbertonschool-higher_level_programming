c21fb4e7aa1c32b87a2268c614eee0d888ff65c8
taret92 <ivancho92.10@hotmail.com>
2022-07-18 15:02:51 -0500

    task 12

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,2 +1,2 @@
---script that upadte the score of bob
-UPDATE second_table SET score = 10 WHERE id = 'Bob'
+-- script that update the score of bob
+UPDATE second_table SET score = 10 WHERE id = 'Bob';
