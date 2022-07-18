123dbb495afabf19e34ff80a2b72ee13eb4eb48b
taret92 <ivancho92.10@hotmail.com>
2022-07-18 14:54:20 -0500

    task 9

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,3 +1,4 @@
+--scrip that create a new table with contain
 CREATE TABLE IF NOT EXISTS second_table(id INT, name VARCHAR(256), score INT);
 INSERT INTO second_table (id, name, score)
 VALUES (1, 'John', 10)
