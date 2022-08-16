b93eb28fc78836d5cb99f27ac6d82e194e32d836
taret92 <ivancho92.10@hotmail.com>
2022-08-16 15:44:03 -0500

    fixed files

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 if (process.argv[2] !== undefined) {
-	console.log(process.argv[2]);
-  } else {
-	console.log('No argument');
-  }
+  console.log(process.argv[2]);
+} else {
+  console.log('No argument');
+}
--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -5,7 +5,7 @@
 can be converted to an integer */
 
 if (parseInt(process.argv[2])) {
-	console.log('My number:', parseInt(process.argv[2]));
-  } else {
-	console.log('Not a number');
-  }
+  console.log('My number:', parseInt(process.argv[2]));
+} else {
+  console.log('Not a number');
+}
