97aa40c3d9114eae89aa22095e84ac42246bb806
taret92 <ivancho92.10@hotmail.com>
2022-08-23 06:53:53 -0500

    task 3

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -2,10 +2,11 @@
 class Rectangle {
   constructor (w, h) {
     if (w > 0 && h > 0) {
-    this.width = w;
-    this.height = h;
+      this.width = w;
+      this.height = h;
     }
   }
+
   print () {
     for (let x = 0; x < this.height; x++) {
       console.log('X'.repeat(this.width));
