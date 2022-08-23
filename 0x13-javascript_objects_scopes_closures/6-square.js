affed07290af6cd98c4c7c0f7258a6e1e1e0872b
taret92 <ivancho92.10@hotmail.com>
2022-08-23 07:11:24 -0500

    task 6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+class Square extends require('./4-rectangle') {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c === undefined) {
+    this.print();
+  } else {
+    for (let x = 0; x < this.height; x++) {
+      console.log(c.repeat(this.width));
+  }
+  }
+  }
+  }
+
+module.exports = Square;
