c4e7d9fde729485f811a62cbb92e0b2c872aaf7a
taret92 <ivancho92.10@hotmail.com>
2022-09-02 05:41:06 -0500

    task 3 fixed

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Script that takes in a URL and displays all HTTP methods the server will accept.
-curl -sI $1 | grep -i Allow | awk '{for(i=1;i<=NF;i++) printf $i" "; print ""}'
+# Script that takes an URL and shows the Allowed OPTIONS
+curl -sI -X OPTIONS "$1" | grep "Allow:" | cut -d " " -f 2-
