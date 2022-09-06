439fbe1ddec18d616d376bb340eb00c9a01dbaeb
taret92 <ivancho92.10@hotmail.com>
2022-09-05 22:30:05 -0500

    task 3 y 4

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,18 +1,22 @@
 #!/usr/bin/python3
 """script that lists all states from
 the database hbtn_0e_0_usa"""
-
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    database = MySQLdb.connect('localhost', argv[1], argv[2], argv[3])
-
-    mycursor = database.cursor()
-    mycursor.execute("SELECT * FROM states WHERE name LIKE BINARY %s \
-                        ORDER BY id ASC", (argv[4], ))
-    result = mycursor.fetchall()
-    for result in result:
-        print(result)
-    mycursor.close()
-    database.close()
+    """main function"""
+    db = MySQLdb.connect(
+            host="localhost",
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3],
+            charset="utf8")
+    data = {'var1': sys.argv[4]}
+    cur = db.cursor()
+    cur.execute("""SELECT * FROM states WHERE name LIKE \
+            BINARY %(var1)s ORDER BY id ASC""", data)
+    for state in cur.fetchall():
+        print(state)
+    cur.close()
+    db.close()
