c706ef67788760dce7ddb232a8187f517464cf05
taret92 <ivancho92.10@hotmail.com>
2022-09-05 22:10:15 -0500

    fixed task

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -5,9 +5,12 @@ import sys
 import MySQLdb
 
 if __name__ == '__main__':
+    """main function"""
     db = MySQLdb.connect(user=sys.argv[1],
-                passwd=sys.argv[2],
-                db=sys.argv[3], port=3306)
+                         passwd=sys.argv[2],
+                         db=sys.argv[3],
+                         host='localhost',
+                         port=3306)
 
     cur = db.cursor()
     cur.execute("SELECT * FROM states WHERE name = %s;", (sys.argv[4],))
--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -6,9 +6,12 @@ import sys
 import MySQLdb
 
 if __name__ == '__main__':
+    """main function"""
     db = MySQLdb.connect(user=sys.argv[1],
-                    passwd=sys.argv[2],
-                    db=sys.argv[3], port=3306)
+                         passwd=sys.argv[2],
+                         db=sys.argv[3],
+                         host='localhost',
+                         port=3306)
 
     cur = db.cursor()
     cur.execute("SELECT cities.id, cities.name, states.name \
